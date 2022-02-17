ada2af19c9d66f2045decdc84b9ce6d9ebd9f846
Zhbert <zhbert@yandex.ru>
2022-02-17 22:13:24 +0300

    Add checker service

--- src/main/java/ru/zhbert/docslinter/Main.java
+++ src/main/java/ru/zhbert/docslinter/Main.java
@@ -1,8 +1,22 @@
 package ru.zhbert.docslinter;
 
+import ru.zhbert.docslinter.service.FileChecker;
+
+import java.io.*;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+    public static void main(String[] args) throws IOException {
+        System.out.println("Docs Linter v.1.0");
+        System.out.println("Started at " + new Date().toString());
+        ArrayList<String> dict = new ArrayList<String>();
+        dict.add("web-интерфейс");
+        FileChecker fileChecker = new FileChecker(dict);
+        File file = new File("/home/zhbert/testfile");
+        fileChecker.checkFile(file);
+        }
 }
