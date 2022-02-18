1defefed3abaefc2a4b92e4ae336c5b0b2274bfe
Zhbert <zhbert@yandex.ru>
2022-02-18 19:54:31 +0300

    Add max lines of file counter

--- src/main/java/ru/zhbert/docslinter/service/FileChecker.java
+++ src/main/java/ru/zhbert/docslinter/service/FileChecker.java
@@ -1,15 +1,13 @@
 package ru.zhbert.docslinter.service;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class FileChecker {
 
     ArrayList<String> dict;
     int dictMaxLen = 0;
+    Integer maxLines = 0;
 
     public FileChecker(ArrayList<String> dict) {
         this.dict = dict;
@@ -22,10 +20,12 @@ public class FileChecker {
 
     public void checkFile(File file) throws IOException {
 
+        maxLines = getLinesCount(file);
         int currentLine = 0;
         String Format = "| %-" + dictMaxLen +"s | %-" + dictMaxLen + "s |%n";
 
         System.out.println("Checking file: " + file.getName());
+        System.out.println(maxLines);
 
         FileReader fr = new FileReader(file);
         BufferedReader reader = new BufferedReader(fr);
@@ -41,6 +41,16 @@ public class FileChecker {
             }
             line = reader.readLine();
         }
+    }
+
+    private Integer getLinesCount(File file) throws IOException {
+        int count = 0;
+        FileReader fr = new FileReader(file);
+        LineNumberReader lnr = new LineNumberReader(fr);
+        while (lnr.readLine() != null) {
+            count++;
+        }
+        return count;
     }
 
     public ArrayList<String> getDict() {
