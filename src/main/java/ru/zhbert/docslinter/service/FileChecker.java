1661619c19feb97b2740d8daf3619e8452e0d2d6
Zhbert <zhbert@yandex.ru>
2022-02-19 18:58:40 +0300

    Add line numbers in table

--- src/main/java/ru/zhbert/docslinter/service/FileChecker.java
+++ src/main/java/ru/zhbert/docslinter/service/FileChecker.java
@@ -7,7 +7,7 @@ public class FileChecker {
 
     ArrayList<String> dict;
     int dictMaxLen = 0;
-    Integer maxLines = 0;
+    int maxLines = 0;
 
     public FileChecker(ArrayList<String> dict) {
         this.dict = dict;
@@ -21,11 +21,10 @@ public class FileChecker {
     public void checkFile(File file) throws IOException {
 
         maxLines = getLinesCount(file);
-        int currentLine = 0;
-        String Format = "| %-" + dictMaxLen +"s | %-" + dictMaxLen + "s |%n";
+        Integer currentLine = 0;
+        String Format = "| %-5s | %-" + dictMaxLen +"s | %-" + dictMaxLen + "s |%n";
 
-        System.out.println("Checking file: " + file.getName());
-        System.out.println(maxLines);
+        System.out.println("Checking file: " + file.getName() + " from " + file.getParent() + "...");
 
         FileReader fr = new FileReader(file);
         BufferedReader reader = new BufferedReader(fr);
@@ -36,7 +35,7 @@ public class FileChecker {
                 if (line.toLowerCase().contains(word.toLowerCase())) {
                     int start = line.toLowerCase().indexOf(word.toLowerCase());
                     int end = start + word.length();
-                    System.out.format(Format, word, line.substring(start, end));
+                    System.out.format(Format, currentLine.toString(), word, line.substring(start, end));
                 }
             }
             line = reader.readLine();
